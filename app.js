59f38d152989bc9b101df6ad8ab470bbd59c1c11
unknown <furqannadeem410@gmail.com>
2025-11-27 01:48:37 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// Q#01 Declare 3 variables in one statement.
+
+// var name, age, country;
+
+// Q# 02  Declare 5 legal & 5 illegal variable names.
+
+// legal variable names...
+
+// var userName;
+// var $Dollar ;
+// var _age ;
+// var my1stproject;
+
+// // illegal variable names...
+
+//  var 1num ;  // start me num nh hota
+//  var user-name ; // hypen allowed nh h
+//  var var ; // Java script ke key word ke name se nh bana sakte example(if,var.let)
+//  var my project ; //space allowed nhi h
